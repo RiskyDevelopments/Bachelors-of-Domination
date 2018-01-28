cd7c7cc54ec428d976ed7eb4bb41ab96eba683e1
Thomas Lee <thomasslee97@gmail.com>
2018-01-28 02:03:51 +0000

    Added JSON representation of classe

--- core/src/sepr/game/saveandload/GameState.java
+++ core/src/sepr/game/saveandload/GameState.java
@@ -15,10 +15,6 @@ import java.util.List;
 
 public class GameState {
     private TurnPhaseType currentPhase;
-    private HashMap<TurnPhaseType, Phase> phases;
-    private SpriteBatch gameplayBatch;
-    private OrthographicCamera gameplayCamera;
-    private Viewport gameplayViewport;
     private Map map;
     private HashMap<Integer, Player> players;
     private boolean turnTimerEnabled;
@@ -26,13 +22,9 @@ public class GameState {
     private long turnTimeStart;
     private List<Integer> turnOrder;
     private int currentPlayerPointer;
-    private Texture mapBackground;
-    private boolean gameSetup = false;
 
     public class MapState {
         public HashMap<Integer, Sector> sectors;
-        public BitmapFont font;
-        public GlyphLayout layout;
     }
 
     public class OptionsState {
@@ -48,7 +40,7 @@ public class GameState {
         public PlayerType playerType;
     }
 
-    public class Sector {
+    public class SectorState {
         public int id;
         public int ownerId;
         public String displayName;
@@ -58,11 +50,55 @@ public class GameState {
         public boolean neutral; // is this sector a default neutral sector
         public int[] adjacentSectorIds; // ids of sectors adjacent to this one
         public Texture sectorTexture;
-        public Pixmap sectorPixmap; // the pixel data of this sectors texture
         public int sectorCentreX; // the centre x coordinate of this sector, relative to the sectorTexture
         public int sectorCentreY; //the centre y coordinate of this sector, relative to the sectorTexture
         public boolean decor; // is this sector for visual purposes only, i.e. lakes are decor
         public String fileName;
         public boolean allocated; // becomes true once the sector has been allocated
+    }
+}
+
+public static final class JSONifier {
+
+}
+
+public class GameStateJSON {
+    public class MapStateJSON {
+        SectorHashMapJSON[] sectors;
+    }
+
+    public class PlayerStateJSON {
+        public int id;
+        public String collegeName;
+        public String playerName;
+        public int troopsToAllocate;
+        public ColorJSON sectorColour;
+        public String playerType;
+    }
+
+    public class SectorHashMapJSON {
+        public int id;
+        public SectorStateJSON sector;
+    }
+
+    public class SectorStateJSON {
+        public int id;
+        public int ownerId;
+        public String displayName;
+        public int unitsInSector;
+        public int reinforcementsProvided;
+        public String college; // name of the college this sector belongs to
+        public boolean neutral; // is this sector a default neutral sector
+        public int[] adjacentSectorIds; // ids of sectors adjacent to this one
+        public String sectorTexture;
+        public int sectorCentreX; // the centre x coordinate of this sector, relative to the sectorTexture
+        public int sectorCentreY; //the centre y coordinate of this sector, relative to the sectorTexture
+        public boolean decor; // is this sector for visual purposes only, i.e. lakes are decor
+        public String fileName;
+        public boolean allocated; // becomes true once the sector has been allocated
+    }
+
+    public class ColorJSON {
+        
     }
 }
